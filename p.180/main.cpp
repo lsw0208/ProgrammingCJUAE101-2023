ba4ab7cfcc0e6b323268632497d63d0f28fd1912
lsw0208 <sang1wona@naver.com>
2023-10-05 18:55:00 +0900

    p.180
    p.180

--- p.180/main.cpp
+++ p.180/main.cpp
@@ -2,15 +2,31 @@
 
 int main(void)
 {
-	int num1 = 0, num2 = 0, res = 0;
-	char op;
+	int num1 = 0;
+	int num3 = 0;
+	int res = 0;
+	char oper;
 
 	printf("사칙연산 입력(정수) :");
-	scanf_s(" % d % c % d", &num1, &op, &num2);
+	scanf("%d %c %d", &num1, &oper, &num3);
 
-	if(op == '+')
+	switch (oper)
+	{
+	case '+':
+		res = num1 + num3;
+		break;
+	case '-':
+		res = num1 - num3;
+		break;
+	case '*':
+		res = num1 * num3;
+		break;
+	case '/':
+		res = num1 / num3;
+		break;
+	}
 
-	printf(" % d % c % d = %d/n", num1, op, num2, res);
+	printf("%d %c %d = %d\n", num1, oper, num3, res);
 
 	return 0;
 }
